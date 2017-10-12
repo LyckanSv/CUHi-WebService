fbb31a8356c6dfb5a0a4ef09ecaee2690795435d
Cesar Callejas <cesar@localhost.localdomain>
2017-10-12 11:48:18 -0600

    Proyecto base

---
+++ resources/views/layout/panel-base-menu.blade.php
@@ -0,0 +1,19 @@
+  <ul class="sidebar-menu" data-widget="tree">
+    <li class="header">Opciones</li>
+    <!-- Optionally, you can add icons to the links -->
+    <li class="active"><a href="#"><i class="fa fa-home"></i> <span>Inicio</span></a></li>
+
+
+    <!-- Histories -->
+    <li class="treeview">
+      <a href="#"><i class="fa fa-link"></i> <span>Historias</span>
+        <span class="pull-right-container">
+            <i class="fa fa-angle-left pull-right"></i>
+          </span>
+      </a>
+      <ul class="treeview-menu">
+        <li><a href="#">Agregar historia</a></li>
+        <li><a href="#">Actualizar historia</a></li>
+      </ul>
+    </li>
+  </ul>
