40e93e6d5cc8889b4aa1d5a55c6239a5a8364be2
Cesar Callejas <cesar@localhost.localdomain>
2017-10-13 14:21:05 -0600

    Cooreccion

--- resources/views/layout/panel-base.blade.php
+++ resources/views/layout/panel-base.blade.php
@@ -227,41 +227,41 @@ desired effect
 <!-- REQUIRED JS SCRIPTS -->
 
 <!-- jQuery 3 -->
-<script src="{{'bower_components/jquery/dist/jquery.min.js')}}"></script>
+<script src="{{asset('bower_components/jquery/dist/jquery.min.js')}}"></script>
 <!-- Bootstrap 3.3.7 -->
-<script src="{{'bower_components/bootstrap/dist/js/bootstrap.min.js')}}"></script>
-<script src="{{'bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js')}}"></script>
+<script src="{{asset('bower_components/bootstrap/dist/js/bootstrap.min.js')}}"></script>
+<script src="{{asset('bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js')}}"></script>
 <!-- AdminLTE App -->
-<script src="{{'dist/js/adminlte.min.js')}}"></script>
-<script src="{{'bower_components/select2/dist/js/select2.full.min.js')}}"></script>
+<script src="{{asset('dist/js/adminlte.min.js')}}"></script>
+<script src="{{asset('bower_components/select2/dist/js/select2.full.min.js')}}"></script>
 <!-- InputMask -->
-<script src="{{'plugins/input-mask/jquery.inputmask.js')}}"></script>
-<script src="{{'plugins/input-mask/jquery.inputmask.date.extensions.js')}}"></script>
-<script src="{{'plugins/input-mask/jquery.inputmask.extensions.js')}}"></script>
+<script src="{{asset('plugins/input-mask/jquery.inputmask.js')}}"></script>
+<script src="{{asset('plugins/input-mask/jquery.inputmask.date.extensions.js')}}"></script>
+<script src="{{asset('plugins/input-mask/jquery.inputmask.extensions.js')}}"></script>
 <!-- date-range-picker -->
-<script src="{{'bower_components/moment/min/moment.min.js')}}"></script>
-<script src="{{'bower_components/bootstrap-daterangepicker/daterangepicker.js')}}"></script>
+<script src="{{asset('bower_components/moment/min/moment.min.js')}}"></script>
+<script src="{{asset('bower_components/bootstrap-daterangepicker/daterangepicker.js')}}"></script>
 <!-- bootstrap datepicker -->
-<script src="{{'bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js')}}"></script>
+<script src="{{asset('bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js')}}"></script>
 <!-- bootstrap color picker -->
-<script src="{{'bower_components/bootstrap-colorpicker/dist/js/bootstrap-colorpicker.min.js')}}"></script>
+<script src="{{asset('bower_components/bootstrap-colorpicker/dist/js/bootstrap-colorpicker.min.js')}}"></script>
 <!-- bootstrap time picker -->
-<script src="{{'plugins/timepicker/bootstrap-timepicker.min.js')}}"></script>
+<script src="{{asset('plugins/timepicker/bootstrap-timepicker.min.js')}}"></script>
 <!-- SlimScroll -->
-<script src="{{'bower_components/jquery-slimscroll/jquery.slimscroll.min.js')}}"></script>
+<script src="{{asset('bower_components/jquery-slimscroll/jquery.slimscroll.min.js')}}"></script>
 <!-- iCheck 1.0.1 -->
-<script src="{{'plugins/iCheck/icheck.min.js')}}"></script>
+<script src="{{asset('plugins/iCheck/icheck.min.js')}}"></script>
 <!-- FastClick -->
-<script src="{{'bower_components/fastclick/lib/fastclick.js')}}"></script>
+<script src="{{asset('bower_components/fastclick/lib/fastclick.js')}}"></script>
 <!-- Table -->
-<script src="{{'../../bower_components/datatables.net/js/jquery.dataTables.min.js')}}"></script>
+<script src="{{asset('../../bower_components/datatables.net/js/jquery.dataTables.min.js')}}"></script>
 
-<script src="{{'../../bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js')}}"></script>
+<script src="{{asset('../../bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js')}}"></script>
 
 <!-- AdminLTE App -->
-<script src="{{'dist/js/adminlte.min.js')}}"></script>
+<script src="{{asset('dist/js/adminlte.min.js')}}"></script>
 <!-- AdminLTE for demo purposes -->
-<script src="{{'dist/js/demo.js')}}"></script>
+<script src="{{asset('dist/js/demo.js')}}"></script>
 
 <script>
   $(function () {
